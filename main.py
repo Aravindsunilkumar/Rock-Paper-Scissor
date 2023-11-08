5893bd28cf845b0c0adbe6bb90c9c69fa42fc5fd
Aravind Sunil Kumar <Aravindsunilkumar@users.noreply.github.com>
2023-11-08 20:36:33 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+game=[rock,paper,scissors]
+user=int(input("0 for Rock, 1 for Paper, 2 for Scissors \n"))
+chosen_name=game[user]
+print(chosen_name)
+print("Computer Chose")
+choice=random.randint(0,2)
+computer = game[choice]
+print(computer)
+if user >= 3 or user < 0:
+  print("Try again")
+elif user == 0 and choice == 2:
+  print("You Win")
+elif user == 2 and choice == 0:
+  print("You Lose")
+elif choice > user:
+  print("You Lose")
+elif user > choice:
+  print("you Win")
+elif user == choice:
+  print("you draw")
